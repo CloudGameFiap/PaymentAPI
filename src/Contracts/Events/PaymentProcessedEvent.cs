2c5c18393208b4c5c041e53f2dea27a2b78b68be
Daniel194 <daniel.rsantos@sptech.school>
2026-07-09 18:18:58 -0300

    fix: arrumando metodos para consumir e publicar os eventos

--- src/Contracts/Events/PaymentProcessedEvent.cs
+++ src/Contracts/Events/PaymentProcessedEvent.cs
@@ -1,12 +1,12 @@
 namespace CloudGame.Contracts.Events;
 
-public sealed record PaymentProcessedEvent
+public record PaymentProcessedEvent
 {
-    public Guid PaymentId { get; init; }
-    public Guid OrderId { get; init; }
-    public int UserId { get; init; }
-    public int GameId { get; init; }
-    public decimal Price { get; init; }
-    public string Status { get; init; } = string.Empty;
-    public DateTime ProcessedAt { get; init; }
+    public Guid PaymentId { get; set; }
+    public Guid OrderId { get; set; }
+    public int UserId { get; set; }
+    public int GameId { get; set; }
+    public decimal Price { get; set; }
+    public string Status { get; set; } = string.Empty;
+    public DateTime ProcessedAt { get; set; }
 }
